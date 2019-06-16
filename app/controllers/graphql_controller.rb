0f381ba87acd49e2414267755a04c8e5bc76c37d
Pandhu Hutomo Aditya <pandhu.hutomo@bukalapak.com>
2019-06-16 10:12:16 +0700

    add option

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,6 +1,12 @@
 class GraphqlController < ApplicationController
   use Rack::PostBodyContentTypeParser
 
+  options "*" do
+    response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+    200
+  end
+
   post '/' do
     result = Schemas.execute(
       params[:query],
